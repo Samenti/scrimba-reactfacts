eeccee1c2b4cdba905d9394b0bbf80e3ea31d05c
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2022-02-01 19:37:42 +0200

    ReactFacts project start

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <>
+    <div className="container">
       <Navbar />
       <Main />
-    </>
+    </div>
   );
 };
