ad733e646e9348914181a5fc284131e5802dd61e
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2022-02-02 14:44:18 +0200

    bullet style, background image; project done

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,7 +10,7 @@ export default function Main() {
           <li>Was originally created by Jordan Walke</li>
           <li>Has well over 100K stars on GitHub</li>
           <li>Is maintained by Facebook</li>
-          <li>Powers thousands of enterprise apps, including<br/>
+          <li>Powers thousands of enterprise apps, including<br />
             mobile apps
           </li>
         </ul>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import reactLogo from "../images/react-icon-small.png";
 export default function Navbar() {
   return (
     <nav className="nav-container">
-      <img className="react-logo" src={reactLogo}></img>
+      <img className="react-logo" src={reactLogo} />
       <h3>ReactFacts</h3>
       <p className="nav-text">React Course - Project 1</p>
     </nav>
