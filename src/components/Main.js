d624e9e779c299922e7b71d43485e95d1071b800
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2022-02-02 04:03:12 +0200

    main content and styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,19 @@ import React from "react";
 
 export default function Main() {
   return (
-    <h1>Main content goes here</h1>
+    <div className="main-container">
+      <main className="main-content">
+        <h1>Fun facts about React</h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including<br/>
+            mobile apps
+          </li>
+        </ul>
+      </main>
+    </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import reactLogo from "../images/react-icon-small.png";
 
 export default function Navbar() {
   return (
-    <nav className="nav">
+    <nav className="nav-container">
       <img className="react-logo" src={reactLogo}></img>
       <h3>ReactFacts</h3>
-      <p className="blurb">React Course - Project 1</p>
+      <p className="nav-text">React Course - Project 1</p>
     </nav>
   );
 };
